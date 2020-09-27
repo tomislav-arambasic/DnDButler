3802ca63adc0ae5c5c7954840bf02c917747cd54
Tomislav Arambasic <tomislav.arambasic@five.agency>
2020-09-27 14:09:35 +0200

    Merge screwup fix

--- src/modules/player/screens/ProfileScreen.js
+++ src/modules/player/screens/ProfileScreen.js
@@ -1,21 +1,27 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Alert, Text, Button} from 'react-native';
 import 'react-native-gesture-handler';
 import {CharacterProfile, CharacterCreator} from '../components';
 
 class ProfileScreen extends Component {
   render() {
+    const {navigation} = this.props;
+
     return (
       <View style={styles.container}>
         {/* <CharacterProfile /> */}
-        <CharacterCreator />
+        {/* <CharacterCreator /> */}
+        <Button
+          title="Create character"
+          onPress={() => navigation.navigate('CreatorBasicInfo')}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1},
+  container: {flex: 1, paddingTop: 200},
 });
 
 export default ProfileScreen;
